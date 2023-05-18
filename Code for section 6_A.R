acf4270f1b26aa84ce94489bba5ceaad18076176
Burak Kucuktopal <131784330+BurakKTopal@users.noreply.github.com>
2023-05-18 12:43:23 +0200

    Update Code for section 6_A.R

--- Code for section 6_A.R
+++ Code for section 6_A.R
@@ -1,9 +1,9 @@
 library(glmnet)
 #To fit the model
-model <- glmnet(X, Y, alpha = 0,lambda = 0.5)
+model <- glmnet(X, Y, alpha = 0,lambda = 1)
 #plotting the coefficients
 plot(coef(model), xlab = 'gene number', ylab = 'Weight of the corresponding gene number')
-y_predicted <- predict(model, s = 1278, newx = X)
+y_predicted <- predict(model, s = 1, newx = X)
 #calculating the SSR:
 ssr <- sum((y_predicted - mean(Y))^2)
 #calculating the SST:
