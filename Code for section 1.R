b0e20c6e03b5ec4ce6ee0de9cc770c35000f6cc3
Burak Kucuktopal <131784330+BurakKTopal@users.noreply.github.com>
2023-05-18 11:55:32 +0200

    Update Code for section 1.R

--- Code for section 1.R
+++ Code for section 1.R
@@ -6,6 +6,3 @@ plot(weight~length, data= example,xlim = c(150,200), ylim = c(60,80), xlab = 'le
 train <- data.frame(length_train = c(170,172), weight_train = c(70,72))
 model<-lm(weight_train ~length_train, data = train)
 abline(model, col = 2)
-
-
-
